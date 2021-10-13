68777f1382e99f0aee59209e32c167ae750d3238
DrissJD <85832488+KingLegend12@users.noreply.github.com>
2021-10-13 23:58:12 +0100

    Run time calculation
    Added Run time calculation for each algorithm

--- Assets/Pathfinding.cs
+++ Assets/Pathfinding.cs
@@ -2,33 +2,58 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 public class Pathfinding : MonoBehaviour {
 
 	public Transform seekertargetAstar, targetAstar,seekertargetUCS, targetUCS,seekertargetBFS, targetBFS, seekertargetDFS, targetDFS;
+	public Stopwatch timerAstar = new Stopwatch();
+	public Stopwatch timerUCS = new Stopwatch();
+	public Stopwatch timerBFS = new Stopwatch();
+	public Stopwatch timerDFS = new Stopwatch();
 	Grid grid;
 
 	void Awake() {
 		grid = GetComponent<Grid> ();
 	}
 
-	void Update() {
-		FindPath (seekertargetAstar.position, targetAstar.position);
-		FindPathUCS (seekertargetUCS.position, targetUCS.position);
-		FindPathBFS (seekertargetBFS.position, targetBFS.position);
-		FindPathDFS (seekertargetDFS.position, targetDFS.position);
+	void Update() {  //remove below comments to update the path 
+	//FindPath (seekertargetAstar.position, targetAstar.position);
+	//	FindPathUCS (seekertargetUCS.position, targetUCS.position);
+	//	FindPathBFS (seekertargetBFS.position, targetBFS.position);
+	//	FindPathDFS (seekertargetDFS.position, targetDFS.position);
 
 		
 	}
+	void Start() { //only prints the times once at after the completion of each algorithm
+		FindPath (seekertargetAstar.position, targetAstar.position);
+		Debug.Log("A* runtime:"+timerAstar.Elapsed.ToString());
+		FindPathUCS (seekertargetUCS.position, targetUCS.position);
+		Debug.Log("UCS runtime:"+timerUCS.Elapsed.ToString());
+		FindPathBFS (seekertargetBFS.position, targetBFS.position);
+		Debug.Log("BFS runtime:"+timerBFS.Elapsed.ToString());
+		FindPathDFS (seekertargetDFS.position, targetDFS.position);
+		Debug.Log("DFS runtime:"+timerDFS.Elapsed.ToString());
 
+		
+		
+		
+		
+		
+	}
 	void FindPath(Vector3 startPos, Vector3 targetPos) {
+		
 		Node startNode = grid.NodeFromWorldPoint(startPos);
 		Node targetNode = grid.NodeFromWorldPoint(targetPos);
 
 		List<Node> openSet = new List<Node>();
 		HashSet<Node> closedSet = new HashSet<Node>();
+		timerAstar.Start();
 		openSet.Add(startNode);
-
+        
+		
 		while (openSet.Count > 0) {
+			
 			Node node = openSet[0];
 			for (int i = 1; i < openSet.Count; i ++) {
 				if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) {
@@ -41,7 +66,9 @@ public class Pathfinding : MonoBehaviour {
 			closedSet.Add(node);
 
 			if (node == targetNode) {
+				timerAstar.Stop();
 				RetracePath(startNode,targetNode);
+				
 				return;
 			}
 
@@ -61,6 +88,8 @@ public class Pathfinding : MonoBehaviour {
 				}
 			}
 		}
+		timerAstar.Stop();
+		
 	}
 
 void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
@@ -69,8 +98,9 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 
 		List<Node> openSet = new List<Node>();
 		HashSet<Node> closedSet = new HashSet<Node>();
+		timerUCS.Start();
 		openSet.Add(startNode);
-
+		
 		while (openSet.Count > 0) {
 			Node node = openSet[0];
 			for (int i = 1; i < openSet.Count; i ++) {
@@ -84,6 +114,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 			closedSet.Add(node);
 
 			if (node == targetNode) {
+				timerUCS.Stop();
 				RetracePathUCS(startNode,targetNode);
 				return;
 			}
@@ -93,7 +124,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 					continue;
 				}
 
-				int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+				int newCostToNeighbour = node.gCost + GetDistanceUCS(node, neighbour);
 				if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
 					neighbour.gCost = newCostToNeighbour;
 					neighbour.parent = node;
@@ -103,6 +134,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 				}
 			}
 		}
+		timerUCS.Stop();
 	}
      
 
@@ -111,11 +143,13 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		Node targetNode = grid.NodeFromWorldPoint(targetPos);
       Queue<Node> queueBFS = new Queue<Node>();
 	  HashSet<Node> closedSet = new HashSet<Node>();
+	  timerBFS.Start();
 		queueBFS.Enqueue(startNode);
         
 		while(queueBFS.Count!=0) { 
 			Node currentNode= queueBFS.Dequeue();
 			if (currentNode == targetNode) {
+				timerBFS.Stop();
 				RetracePathBFS(startNode,targetNode);
 				return;
 			}
@@ -132,6 +166,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 				}
 		}
 		}
+		timerBFS.Stop();
 	}
 
 	void FindPathDFS(Vector3 startPos, Vector3 targetPos) { 
@@ -139,6 +174,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		Node targetNode = grid.NodeFromWorldPoint(targetPos);
       Stack<Node> StackDFS = new Stack<Node>();
 	  HashSet<Node> closedSet = new HashSet<Node>();
+	    timerDFS.Start();
 		StackDFS.Push(startNode);
         
 		while(StackDFS.Count!=0) { 
@@ -160,6 +196,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 				}
 		}
 		}
+		timerDFS.Stop();
 	}
 
 	void RetracePath(Node startNode, Node endNode) {
@@ -221,5 +258,13 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		if (dstX > dstY)
 			return 14*dstY + 10* (dstX-dstY);
 		return 14*dstX + 10 * (dstY-dstX);
+	}
+	int GetDistanceUCS(Node nodeA, Node nodeB) {
+		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		if (dstX > dstY)
+			return 10*dstY + 10* (dstX-dstY);
+		return 10*dstX + 10 * (dstY-dstX);
 	}
 }
