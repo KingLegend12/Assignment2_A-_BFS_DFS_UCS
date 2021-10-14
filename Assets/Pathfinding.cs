c63add4686cd42c8f1dc56cf8f0e1125de1a0dc6
DrissJD <85832488+KingLegend12@users.noreply.github.com>
2021-10-14 23:52:35 +0100

    updated old files
    added run time, memory usage output, and more comments

--- Assets/Pathfinding.cs
+++ Assets/Pathfinding.cs
@@ -1,3 +1,24 @@
+/*
+File		: Pathfinding.cs
+Created by	: Driss Jaidi and Mohammed Chaouni
+Version #	: 4.0
+Updated on 	: October 14, 2021
+References: { Sebastian Lague Youtube channel and github code
+ https://www.youtube.com/watch?v=-L-WgKMFuhE&list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW  
+ https://github.com/SebLague/Pathfinding
+ 			}
+This program contains 10 functions:
+FindPath: finds shortest path using A*, the code can be modified for different A* heuristics
+FindPathUCS: finds shortest path using uniform cost search, the code can be modified for different gCosts
+FindPathBFS: finds shortest path using Breadth first search.
+FindPathDFS: finds shortest path using Depth first search.
+RetracePath: marks the nodes that constitue the shortest path of A*
+RetracePathUCS: marks the nodes that constitue the shortest path of A*
+RetracePathBFS: marks the nodes that constitue the shortest path of A*
+RetracePathDFS: marks the nodes that constitue the shortest path of A*
+GetDistance: gets the optimal distance between the two parameters of the function considering that some directions have a higher cost that normal movement
+GetDistanceUCS: gets the optimal distance between the two parameters of the function considering a straight line distance using sqrt(par1, par2) all to the power two
+*/
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +32,7 @@ public class Pathfinding : MonoBehaviour {
 	public Stopwatch timerUCS = new Stopwatch();
 	public Stopwatch timerBFS = new Stopwatch();
 	public Stopwatch timerDFS = new Stopwatch();
+	public int totalMemoryUsedAstar = 0,  totalMemoryUsedUCS = 0, totalMemoryUsedBFS = 0, totalMemoryUsedDFS = 0;
 	Grid grid;
 
 	void Awake() {
@@ -18,22 +40,25 @@ public class Pathfinding : MonoBehaviour {
 	}
 
 	void Update() {  //remove below comments to update the path 
-	//FindPath (seekertargetAstar.position, targetAstar.position);
-	//	FindPathUCS (seekertargetUCS.position, targetUCS.position);
-	//	FindPathBFS (seekertargetBFS.position, targetBFS.position);
-	//	FindPathDFS (seekertargetDFS.position, targetDFS.position);
+	
+		//FindPath (seekertargetAstar.position, targetAstar.position);
 
+	
+		//FindPathUCS (seekertargetUCS.position, targetUCS.position);
+		//FindPathBFS (seekertargetBFS.position, targetBFS.position);
+	 	//FindPathDFS (seekertargetDFS.position, targetDFS.position);
+		
 		
 	}
 	void Start() { //only prints the times once at after the completion of each algorithm
 		FindPath (seekertargetAstar.position, targetAstar.position);
-		Debug.Log("A* runtime:"+timerAstar.Elapsed.ToString());
+		Debug.Log("A* runtime:"+timerAstar.Elapsed.ToString()+"Memory used to find the returned path:"+totalMemoryUsedAstar+"*8 bytes for 32bits/*12-16bytes for 64bits");
 		FindPathUCS (seekertargetUCS.position, targetUCS.position);
-		Debug.Log("UCS runtime:"+timerUCS.Elapsed.ToString());
+		Debug.Log("UCS runtime:"+timerUCS.Elapsed.ToString()+"Memory used to find the returned path:"+totalMemoryUsedUCS+"*8 bytes for 32bits/*12-16bytes for 64bits");
 		FindPathBFS (seekertargetBFS.position, targetBFS.position);
-		Debug.Log("BFS runtime:"+timerBFS.Elapsed.ToString());
+		Debug.Log("BFS runtime:"+timerBFS.Elapsed.ToString()+"Memory used to find the returned path:"+totalMemoryUsedBFS+"*8 bytes for 32bits/*12-16bytes for 64bits");
 		FindPathDFS (seekertargetDFS.position, targetDFS.position);
-		Debug.Log("DFS runtime:"+timerDFS.Elapsed.ToString());
+		Debug.Log("DFS runtime:"+timerDFS.Elapsed.ToString()+"Memory used to find the returned path:"+totalMemoryUsedDFS+"*8 bytes for 32bits/*12-16bytes for 64bits");
 
 		
 		
@@ -66,9 +91,11 @@ public class Pathfinding : MonoBehaviour {
 			closedSet.Add(node);
 
 			if (node == targetNode) {
-				timerAstar.Stop();
-				RetracePath(startNode,targetNode);
 				
+				RetracePath(startNode,targetNode);
+				timerAstar.Stop();
+				//Debug.Log("A* runtime:"+timerAstar.Elapsed.ToString()+"Memory used to find the returned path:"+totalMemoryUsedAstar+"*8 bytes for 32bits/*12-16bytes for 64bits");
+
 				return;
 			}
 
@@ -80,9 +107,9 @@ public class Pathfinding : MonoBehaviour {
 				int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
 				if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
 					neighbour.gCost = newCostToNeighbour;
-					neighbour.hCost = GetDistance(neighbour, targetNode);
+					neighbour.hCost = GetDistance(neighbour, targetNode); //snapshot multiplies this by two or uses the line commented below
+					//neighbour.hCost = GetDistanceUCS(neighbour, targetNode); gets distance in a straight line using sqrt root
 					neighbour.parent = node;
-
 					if (!openSet.Contains(neighbour))
 						openSet.Add(neighbour);
 				}
@@ -114,8 +141,9 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 			closedSet.Add(node);
 
 			if (node == targetNode) {
-				timerUCS.Stop();
+				
 				RetracePathUCS(startNode,targetNode);
+				timerUCS.Stop();
 				return;
 			}
 
@@ -137,7 +165,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		timerUCS.Stop();
 	}
      
-
+ 
 	void FindPathBFS(Vector3 startPos, Vector3 targetPos) { 
 		Node startNode = grid.NodeFromWorldPoint(startPos);
 		Node targetNode = grid.NodeFromWorldPoint(targetPos);
@@ -149,8 +177,8 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		while(queueBFS.Count!=0) { 
 			Node currentNode= queueBFS.Dequeue();
 			if (currentNode == targetNode) {
-				timerBFS.Stop();
 				RetracePathBFS(startNode,targetNode);
+				timerBFS.Stop();
 				return;
 			}
 			closedSet.Add(currentNode);
@@ -180,7 +208,9 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		while(StackDFS.Count!=0) { 
 			Node currentNode= StackDFS.Pop();
 			if (currentNode == targetNode) {
+				
 				RetracePathDFS(startNode,targetNode);
+				timerDFS.Stop();
 				return;
 			}
 			closedSet.Add(currentNode);
@@ -202,8 +232,9 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 	void RetracePath(Node startNode, Node endNode) {
 		List<Node> path = new List<Node>();
 		Node currentNode = endNode;
-
+		
 		while (currentNode != startNode) {
+			totalMemoryUsedAstar=totalMemoryUsedAstar+1;
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
@@ -217,6 +248,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		Node currentNode = endNode;
 
 		while (currentNode != startNode) {
+			totalMemoryUsedUCS=totalMemoryUsedUCS+1;
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
@@ -230,6 +262,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		Node currentNode = endNode;
 
 		while (currentNode != startNode) {
+			totalMemoryUsedBFS=totalMemoryUsedBFS+1;
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
@@ -243,6 +276,7 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 		Node currentNode = endNode;
 
 		while (currentNode != startNode) {
+			totalMemoryUsedDFS=totalMemoryUsedDFS+1;
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
@@ -262,9 +296,12 @@ void FindPathUCS(Vector3 startPos, Vector3 targetPos) {
 	int GetDistanceUCS(Node nodeA, Node nodeB) {
 		int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
 		int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+		/*if (dstX > dstY)
+			return 14*dstY + 10* (dstX-dstY);
+		return 14*dstX + 10 * (dstY-dstX);*/  //same distance as A* gcost
 
-		if (dstX > dstY)
-			return 10*dstY + 10* (dstX-dstY);
-		return 10*dstX + 10 * (dstY-dstX);
+		return (int) Mathf.Sqrt((Mathf.Pow(dstX, 2) + Mathf.Pow(dstY, 2))); //normal straight line distance same in all directions
+	
+		
 	}
 }
